b42565cf27f7fddac706dfffdc5e3322057c34c3
Eylem GÖKDEMİR <eylemgokdemir05@gmail.com>
2022-05-19 19:59:38 +0300

    SSR changes added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ac952a3e2b5802c"; }
+/******/ 		__webpack_require__.h = function() { return "8328f8c36ebcc128"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
